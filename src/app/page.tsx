961481ed08ca3fc76b5ab159efdb6bfec99a3138
gitdeepaks <deepaksankhyan92@outlook.com>
2025-08-10 17:23:48 +0530

    01:background-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,32 @@
+'use client';
+
+import { Button } from '@/components/ui/button';
+import { useTRPC } from '@/trpc/client';
+import { useMutation } from '@tanstack/react-query';
+import { toast } from 'sonner';
+
 const Page = () => {
-  return <div>Hello World</div>;
+  const trpc = useTRPC();
+  const invoke = useMutation(
+    trpc.invoke.mutationOptions({
+      onSuccess: () => {
+        toast.success('Background job invoked');
+      },
+      onError: () => {
+        toast.error('Failed to invoke background job');
+      },
+    })
+  );
+  return (
+    <div className="p-4 max-w-7xl mx-auto">
+      <Button
+        disabled={invoke.isPending}
+        onClick={() => invoke.mutate({ text: 'John Doe' })}
+      >
+        Invoke Background Job
+      </Button>
+    </div>
+  );
 };
 
 export default Page;
